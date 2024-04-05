a1e0d42d6871d1e285fe0dd5c50cb820d5716366
dev-avology <devavology12@gmail.com>
2024-04-05 12:13:41 +0530

    dfdsd

--- js/custom.js
+++ js/custom.js
@@ -25,3 +25,26 @@ function navLinkClick() {
     }
 }
 
+
+// 
+// JavaScript to toggle accordion functionality
+var accordions = document.getElementsByClassName("accordion-heading");
+for (var i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener("click", function () {
+        var isActive = this.classList.contains('active');
+
+        // Close all accordion items
+        var activeItems = document.querySelectorAll('.accordion-heading.active');
+        for (var j = 0; j < activeItems.length; j++) {
+            activeItems[j].classList.remove('active');
+            activeItems[j].nextElementSibling.style.maxHeight = '0';
+        }
+
+        // Toggle current accordion item
+        if (!isActive) {
+            this.classList.add('active');
+            var content = this.nextElementSibling;
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
